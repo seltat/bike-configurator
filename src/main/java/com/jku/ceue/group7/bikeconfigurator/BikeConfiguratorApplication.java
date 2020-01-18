c4211e306963b3f79f198cb788750825d3c4bc33
Felix Winterleitner <f.winterleitner@blitzkabine.com>
2020-01-18 21:55:21 +0100

    created spring project

---
+++ src/main/java/com/jku/ceue/group7/bikeconfigurator/BikeConfiguratorApplication.java
@@ -0,0 +1,13 @@
+package com.jku.ceue.group7.bikeconfigurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeConfiguratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jku/ceue/group7/bikeconfigurator/BikeConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jku.ceue.group7.bikeconfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BikeConfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
