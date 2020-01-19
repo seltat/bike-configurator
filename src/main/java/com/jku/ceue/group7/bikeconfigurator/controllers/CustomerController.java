e705623ef21cda8f92cb11eb0980d040f5bddfd3
chrigHub <51883441+chrigHub@users.noreply.github.com>
2020-01-19 01:17:06 +0100

    Logging process

---
+++ src/main/java/com/jku/ceue/group7/bikeconfigurator/controllers/CustomerController.java
@@ -0,0 +1,35 @@
+package com.jku.ceue.group7.bikeconfigurator.controllers;
+import com.jku.ceue.group7.bikeconfigurator.entities.Customer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class CustomerController {
+    public static final Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+    @PostMapping(value = "/login")
+    public String loginCustomer(@ModelAttribute Customer customer, Model model){
+        log.info(customer.getFname() +" "+ customer.getLname());
+        model.addAttribute("fname", customer.getFname());
+        model.addAttribute("lname", customer.getLname());
+        return "loginResult";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model){
+        model.addAttribute("customer", new Customer());
+        log.info("entered getCall");
+        return "login";
+    }
+
+
+
+}
--- src/main/java/com/jku/ceue/group7/bikeconfigurator/entities/Lenkertyp.java
+++
@@ -1,6 +0,0 @@
-package com.jku.ceue.group7.bikeconfigurator.entities;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import org.springframework.beans.factory.annotation.Value;
-
-
